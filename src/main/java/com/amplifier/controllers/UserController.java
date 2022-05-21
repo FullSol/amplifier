2821da18090051ecc701f2c6516edb043af45211
FullSol <ceraines@student.fullsail.edu>
2022-05-21 17:08:20 -0400

    created servie and dao interface empty files

--- src/main/java/com/amplifier/controllers/UserController.java
+++ src/main/java/com/amplifier/controllers/UserController.java
@@ -1,8 +1,24 @@
 package com.amplifier.controllers;
 
+import com.amplifier.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
 @RestController
+@RequestMapping("/api/v1")
+@Api(value = "UserController", description = "REST controller related to User Entities")
 public class UserController {
 
-    @GetMapping("/users")
-    public void 
+    @Autowired
+    private UserService userService;
+
+    // @ApiOperation(value="Find user by id number", notes="Provide an id to lookup
+    // a specific user from the API", response = User.class)
+    // @GetMapping(path = "/users", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    // public @ResponseBody User getById(@)
+
 }
