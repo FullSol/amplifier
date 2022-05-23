a2727345d555079a9502663ca81e5ae313f9e31e
FullSol <ceraines@student.fullsail.edu>
2022-05-23 00:32:34 -0400

    added getById method

--- src/main/java/com/amplifier/controllers/UserController.java
+++ src/main/java/com/amplifier/controllers/UserController.java
@@ -2,13 +2,18 @@ package com.amplifier.controllers;
 
 import java.util.List;
 
+import com.amplifier.models.ClientMessage;
 import com.amplifier.models.User;
 import com.amplifier.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +28,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @ApiOperation(value = "Find user by id number", notes = "Retrieve all users from the API")
-    @GetMapping(path = "/users", consumes = { MediaType.APPLICATION_JSON_VALUE })
-    public @ResponseBody List<User> users() {
-        return userService.getAll();
+    @ApiOperation(value = "Find user by id number", notes = "Provide an id to lookup a specific user from the API", response = User.class)
+    @GetMapping(path = "/user")
+    public @ResponseBody User getById(@RequestParam(value = "id", name = "id") int id) {
+        System.out.println("TEST: " + userService.getUserById(id));
+        return userService.getUserById(id);
     }
-
 }
