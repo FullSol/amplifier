9de6af7c6f0e4b612430dbeaf03b0f98b8a53d0d
briiitelord <96019784+briiitelord@users.noreply.github.com>
2022-05-24 08:18:36 -0400

    ImgPost services and services implementation interface and class made - CRUD operations

--- src/main/java/com/amplifier/services/ImgPostService.java
+++ src/main/java/com/amplifier/services/ImgPostService.java
@@ -1,5 +1,19 @@
 package com.amplifier.services;
 
+import java.util.List;
+
+import com.amplifier.models.ImgPost;
+
 public interface ImgPostService {
+
+    public List<ImgPost> getAllImgPosts();
+
+    public boolean createImgPost(ImgPost imgPost);
+
+    public ImgPost getImgPostById(int Id);
+
+    public boolean updateImgPost(ImgPost mockImgPostModification);
+
+    public boolean deleteImgPostById(int Id);
 
 }
