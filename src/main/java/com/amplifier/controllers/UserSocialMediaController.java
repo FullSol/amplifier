7c5283dd38ebea1e9a37e1318d09d1eab6f0c817
briiitelord <96019784+briiitelord@users.noreply.github.com>
2022-05-24 13:44:28 -0400

    Update - user social media controller and user social media controller test

--- src/main/java/com/amplifier/controllers/UserSocialMediaController.java
+++ src/main/java/com/amplifier/controllers/UserSocialMediaController.java
@@ -1,5 +1,68 @@
 package com.amplifier.controllers;
 
+import java.util.List;
+
+import com.amplifier.models.ClientMessage;
+import com.amplifier.models.User;
+import com.amplifier.services.UserService;
+
+import com.amplifier.services.UserSocialMediaService;
+import com.amplifier.models.UserSocialMedia;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+
+
+@RestController
+@RequestMapping("/api/v1")
+@Api(value = "UserSocialMediaRestController", description = "REST controller related to User social media Entities")
 public class UserSocialMediaController {
+
+
+    @Autowired
+    private UserSocialMediaService userSocialMediaService;
+
+    @ApiOperation(value = "Find User Social Media by id number", notes = "Provide an id to lookup a specific user social media from the API", response = UserSocialMedia.class)
+    @GetMapping(path = "/user/user_id/social-media/media_id")
+    public @ResponseBody UserSocialMedia getUserSocialMediaById(@RequestParam(value = "id", name = "media_id") int id) {
+        // System.out.println("TEST: " + userSocialMediaService.getUserSocialMediaById(id));
+        return userSocialMediaService.getUserSocialMediaById(id);
+    }
+
+    // @GetMapping("/users")
+    // @ApiOperation(value = "Find all users")
+    // public @ResponseBody List<User> getAll() {
+    //     return userService.getAllUsers();
+    // }
+
+    @PostMapping("/user/user_id/social-media")
+    @ApiOperation(value = "add new user social media entity")
+    public @ResponseBody ClientMessage addUserSocialMedia(@RequestBody UserSocialMedia userSocialMedia) {
+        return userSocialMediaService.addUserSocialMedia(userSocialMedia) ? "CREATION_SUCCESSFUL" : "CREATION_FAILED";
+    }
+
+    @PutMapping("/user/user_id/social-media")
+    @ApiOperation(value = "Update user social media entity")
+    public @ResponseBody ClientMessage updateUserSocialMedia(@RequestBody UserSocialMedia userSocialMedia) {
+        return userSocialMediaService.updateUser(userSocialMedia) ? "UPDATE_SUCCESSFUL" : "UPDATE_FAILED";
+    }
+
+    @DeleteMapping("/user/user_id/social-media")
+    @ApiOperation(value = "Remove user social media entity")
+    public @ResponseBody ClientMessage deleteUserSocialMedia(@RequestBody UserSocialMedia userSocialMedia) {
+        return userSocialMediaService.deleteUserSocialMedia(userSocialMedia) ? "DELETION_SUCCESSFUL" : "DELETION_FAILED";
+    }
 
 }
