9ad28b23bcaab4595afa97ad28f204bcbb6be6fd
L3viathon <lvmchoi@gmail.com>
2022-05-24 11:58:32 -0700

    Added Character repository

--- src/main/java/com/amplifier/repositories/CharacterRepository.java
+++ src/main/java/com/amplifier/repositories/CharacterRepository.java
@@ -1,5 +1,17 @@
 package com.amplifier.repositories;
 
+import java.util.List;
+
 public interface CharacterRepository {
+
+    public List<Character> findAll();
+
+    public boolean create(Character character);
+
+    public Character findById(int id);
+
+    public boolean update(Character character);
+
+    public boolean deleteById(int id);
 
 }
