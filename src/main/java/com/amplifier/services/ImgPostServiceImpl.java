4b54ad19099c1a6eb3da8e0c106fa12ee5a5c34b
briiitelord <96019784+briiitelord@users.noreply.github.com>
2022-05-24 08:19:04 -0400

    ImgPost services and services implementation interface and class made - CRUD operations

---
+++ src/main/java/com/amplifier/services/ImgPostServiceImpl.java
@@ -0,0 +1,45 @@
+package com.amplifier.services;
+
+import java.util.List;
+
+import com.amplifier.models.ImgPost;
+import com.amplifier.repositories.ImgPostRepositoryImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ImgPostServiceImpl implements ImgPostService{
+
+    @Autowired
+    private ImgPostRepositoryImpl imgPostRepository;
+
+    @Override
+    public List<ImgPost> getAllImgPosts() {
+        return imgPostRepository.findAll();
+    }
+
+    @Override
+    public boolean createImgPost(ImgPost imgPost) {
+        return imgPostRepository.create(imgPost);
+
+    }
+
+    @Override
+    public ImgPost getImgPostById(int Id) {
+        return null;
+    }
+
+    @Override
+    public boolean updateImgPost(ImgPost mockImgPostModification) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteImgPostById(int Id) {
+        return false;
+    }
+    
+}
