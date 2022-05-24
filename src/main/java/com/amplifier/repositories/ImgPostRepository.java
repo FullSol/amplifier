e58a26070f4db74603840b65659fdc575df4be7c
FullSol <ceraines@student.fullsail.edu>
2022-05-24 13:27:06 -0400

    fixed some errors in merged code.

--- src/main/java/com/amplifier/repositories/ImgPostRepository.java
+++ src/main/java/com/amplifier/repositories/ImgPostRepository.java
@@ -1,7 +1,17 @@
 package com.amplifier.repositories;
 
+import java.util.List;
+
 import com.amplifier.models.ImgPost;
 
 public interface ImgPostRepository {
-    public ImgPost findAll();
+    public List<ImgPost> findAll();
+
+    public boolean create(ImgPost imgPost);
+
+    public ImgPost findById(int id);
+
+    public boolean update(ImgPost imgPost);
+
+    public boolean deleteById(int id);
 }
--- src/main/java/com/amplifier/repositories/ImgPostRepositoryImpl.java
+++ src/main/java/com/amplifier/repositories/ImgPostRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.amplifier.repositories;
 
+import java.util.List;
+
 import com.amplifier.models.ImgPost;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,27 @@ public class ImgPostRepositoryImpl implements ImgPostRepository {
     private ImgPostRepository imgPostRepository;
 
     @Override
-    public ImgPost findAll() {
+    public List<ImgPost> findAll() {
         return null;
+    }
+
+    @Override
+    public boolean create(ImgPost imgPost) {
+        return false;
+    }
+
+    @Override
+    public ImgPost findById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean update(ImgPost imgPost) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return false;
     }
 }
