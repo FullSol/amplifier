e68ed16c3ebae94837110c1ec144206ecab3ac37
FullSol <ceraines@student.fullsail.edu>
2022-05-24 15:50:55 -0400

    init commit

--- src/main/java/com/amplifier/services/UserSocialMediaService.java
+++ src/main/java/com/amplifier/services/UserSocialMediaService.java
@@ -1,5 +1,15 @@
 package com.amplifier.services;
 
+import com.amplifier.models.UserSocialMedia;
+
 public interface UserSocialMediaService {
+
+    boolean deleteUserSocialMedia(UserSocialMedia userSocialMedia);
+
+    boolean updateUser(UserSocialMedia userSocialMedia);
+
+    UserSocialMedia getUserSocialMediaById(int id);
+
+    boolean addUserSocialMedia(UserSocialMedia userSocialMedia);
 
 }
---
+++ src/main/java/com/amplifier/services/UserSocialMediaServiceImpl.java
@@ -0,0 +1,23 @@
+package com.amplifier.services;
+
+import com.amplifier.models.UserSocialMedia;
+
+public class UserSocialMediaServiceImpl implements UserSocialMediaService {
+
+    public boolean deleteUserSocialMedia(UserSocialMedia userSocialMedia) {
+        return false;
+    };
+
+    public boolean updateUser(UserSocialMedia userSocialMedia) {
+        return false;
+    };
+
+    public UserSocialMedia getUserSocialMediaById(int id) {
+        return null;
+    };
+
+    public boolean addUserSocialMedia(UserSocialMedia userSocialMedia) {
+        return false;
+    };
+
+}
