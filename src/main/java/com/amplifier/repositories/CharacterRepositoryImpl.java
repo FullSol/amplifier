06629e3a514064be97213ea915ff146b058a0827
briiitelord <96019784+briiitelord@users.noreply.github.com>
2022-05-25 15:06:01 -0400

    Character service test done --added repository and service layer for testing without implementations, also model with constructor following DB sketch

---
+++ src/main/java/com/amplifier/repositories/CharacterRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.amplifier.repositories;
+
+import java.util.List;
+
+import com.amplifier.models.Character;
+
+public class CharacterRepositoryImpl implements CharacterRepository{
+
+    @Override
+    public List<Character> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Character getById(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Character getByName(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Character> getAllByRealm(String realm) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean create(Character character) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean update(Character character) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
