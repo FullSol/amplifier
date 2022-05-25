607d95cd240e67a7ce139c7069c9a5837ba3fcc1
pyaeger <patrick540@revature.net>
2022-05-25 12:44:15 -0400

    updated UserRolesService with abstract methods

--- src/main/java/com/amplifier/services/UserRolesService.java
+++ src/main/java/com/amplifier/services/UserRolesService.java
@@ -1,5 +1,21 @@
 package com.amplifier.services;
 
+import java.util.List;
+
+import com.amplifier.models.UserRole;
+
+
 public interface UserRolesService {
+	
+    public List<UserRole> getAllUserRoles();
+
+    public boolean createUserRole(UserRole userRole);
+
+    public UserRole getUserRoleById(int Id);
+
+    public boolean updateUserRole(UserRole userRole);
+
+    public boolean deleteUserRoleById(int Id);
+	
 
 }
