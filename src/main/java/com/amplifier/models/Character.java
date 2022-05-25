ed75b08f36001504ed448493983d933bb62d3fb0
briiitelord <96019784+briiitelord@users.noreply.github.com>
2022-05-25 14:36:44 -0400

    saving progress

--- src/main/java/com/amplifier/models/Character.java
+++ src/main/java/com/amplifier/models/Character.java
@@ -1,5 +1,53 @@
 package com.amplifier.models;
 
+import java.time.LocalDate;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "user_character")
+@Data
+@ApiModel(value = "User_Character", description = "This model serves as the basis for all user character entity API operations")
 public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @ApiModelProperty(name = "id", notes = "An integer value that serves as the unique identifier for any user entity.", required = true, value = "1")
+    private int id;
+
+    @Column(name = "character_realm")
+    @ApiModelProperty(name = "Character_Realm", notes = "A string value that serves as the user character's realm name")
+    private String character_realm;
+
+    @Column(name = "character_name")
+    @ApiModelProperty(name = "Character_Name", notes = "A string value that serves as the user character's name")
+    private String character_name;
+
+    public Character() {
+    }
+
+    public Character(int id, String character_realm, String character_name) {
+        this.id = id;
+        this.character_realm = character_realm;
+        this.character_name = character_name;
+    }
+
+    
+
+    
+
 
 }
--- src/main/java/com/amplifier/repositories/CharacterRepository.java
+++ src/main/java/com/amplifier/repositories/CharacterRepository.java
@@ -1,5 +1,23 @@
 package com.amplifier.repositories;
 
+import java.util.List;
+
+import com.amplifier.models.Character;
+
 public interface CharacterRepository {
+
+    public List<Character> getAll();
+
+    public Character getById(int id);
+
+    public Character getByName(String name);
+
+    public List<Character> getAllByRealm(String realm);
+
+    public boolean create(Character character);
+
+    public boolean update(Character character);
+
+    public boolean delete(int id);
 
 }
