b0b967fd0e8a21b4454e5354cfc9bb265947b77e
pyaeger <patrick540@revature.net>
2022-05-26 15:12:24 -0400

    finished build and pushing

--- src/main/java/com/amplifier/repositories/UserSocialMediaRepository.java
+++ src/main/java/com/amplifier/repositories/UserSocialMediaRepository.java
@@ -1,7 +1,35 @@
 package com.amplifier.repositories;
 
+import java.util.List;
+
+import com.amplifier.models.SocialMedia;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface UserSocialMediaRepository {
 	
-	//now working on this repo.
+    @Query(value = "SELECT * FROM user_social_media", nativeQuery = true)
+    public List<SocialMedia> findAll();
 
+    @Modifying
+	@Query(value= "INSERT INTO user_social_media (twitter_link, facebook_link, instagram_link) VALUES (?1, ?2, ?3)", nativeQuery = true)
+	public boolean create(String twitterLink, String facebookLink, String instagramLink);
+
+	@Query(value = "SELECT * FROM user_social_media WHERE id=?1", nativeQuery = true)
+	public SocialMedia findById(int id);
+	
+	@Modifying
+	@Query(value = "UPDATE user_social_media SET twitter_link=?1, facebook_link=?2, instagram_link=?3 WHERE id=?4", nativeQuery = true)
+	public boolean update(String twitterLink, String facebookLink, String instagramLink, int id);
+	    
+	@Modifying
+	@Query(value = "DELETE * FROM user_social_media WHERE id=?1", nativeQuery = true)
+	public boolean delete(int id);
+    
 }
