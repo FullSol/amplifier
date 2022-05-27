b41320a3eb02a0a2b0377ea3efd6b6bfd8b9b7d4
FullSol <ceraines@student.fullsail.edu>
2022-05-27 15:51:28 -0400

    Add column caption to ImgPost
    - Added the column to ImgPost.
    - Updated method signatures in service and repository layers.
    - Corrected URI in the controller layer (this may be a different issue).

--- src/main/java/com/amplifier/controllers/ImgPostController.java
+++ src/main/java/com/amplifier/controllers/ImgPostController.java
@@ -34,25 +34,25 @@ public class ImgPostController {
     private ImgPostService imgPostService;
 
     @ApiOperation(value = "Find User Social Media by id number", notes = "Provide an id to lookup a specific user social media from the API", response = ImgPost.class)
-    @GetMapping(path = "/user/img-post?id={id}")
+    @GetMapping(path = "/img-post?id={id}")
     public @ResponseBody ImgPost getById(
             @RequestParam(name = "id") int id) {
         return imgPostService.getById(id);
     }
 
-    @PostMapping("/user/img-post")
+    @PostMapping("/img-post")
     @ApiOperation(value = "add new image post entity")
     public @ResponseBody ClientMessage add(@RequestBody ImgPost imgPost) {
         return imgPostService.add(imgPost) ? CREATION_SUCCESSFUL : CREATION_FAILED;
     }
 
-    @PutMapping("/user/img-post")
+    @PutMapping("/img-post")
     @ApiOperation(value = "Update image post entity")
     public @ResponseBody ClientMessage update(@RequestBody ImgPost imgPost) {
         return imgPostService.edit(imgPost) ? UPDATE_SUCCESSFUL : UPDATE_FAILED;
     }
 
-    @DeleteMapping("/user/img-post?id={id}")
+    @DeleteMapping("/img-post?id={id}")
     @ApiOperation(value = "Remove image post entity")
     public @ResponseBody ClientMessage delete(@RequestParam(value = "id") int id) {
         return imgPostService.remove(id) ? DELETION_SUCCESSFUL
--- src/main/java/com/amplifier/repositories/ImgPostRepository.java
+++ src/main/java/com/amplifier/repositories/ImgPostRepository.java
@@ -17,14 +17,14 @@ public interface ImgPostRepository extends JpaRepository<ImgPost, Integer> {
     @Query(value = "SELECT * FROM ImgPosts", nativeQuery = true)
     public List<ImgPost> findAll();
 
-    @Query(value = "INSERT INTO ImgPosts (img_location, author_id) VALUES(?1, ?2)", nativeQuery = true)
-    public boolean create(String imgLocation, int authorId);
+    @Query(value = "INSERT INTO ImgPosts (img_caption, img_location, author_id) VALUES(?1, ?2, ?3)", nativeQuery = true)
+    public boolean create(String imgCaption, String imgLocation, int authorId);
 
     @Query(value = "SELECT * FROM ImgPosts WHERE id=?1")
     public ImgPost findById(int id);
 
-    @Query(value = "UPDATE ImgPosts SET img_location=?1, author_id=?2 WHERE id=?3", nativeQuery = true)
-    public boolean update(String imgLocation, int authorId, int id);
+    @Query(value = "UPDATE ImgPosts SET img_caption, img_caption=?1, img_location=?2, author_id=?3 WHERE id=?3", nativeQuery = true)
+    public boolean update(String imgCaption, String imgLocation, int authorId, int id);
 
     @Query(value = "DELETE * FROM ImgPosts WHERE id=?1", nativeQuery = true)
     public boolean deleteById(int id);
