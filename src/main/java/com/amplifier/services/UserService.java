334e043ecd4ec3cab8e5ec8ba1197b884b4a4d81
L3viathon <lvmchoi@gmail.com>
2022-05-27 17:23:36 -0700

    Updated Service layers with annotations

--- src/main/java/com/amplifier/services/UserService.java
+++ src/main/java/com/amplifier/services/UserService.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import com.amplifier.models.User;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public interface UserService {
     public List<User> getAll();
 
