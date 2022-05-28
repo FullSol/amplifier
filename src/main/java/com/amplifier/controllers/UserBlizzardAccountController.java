4a18ed827ced872f411dbabb31ba4f7aed34d5c6
FullSol <ceraines@student.fullsail.edu>
2022-05-27 20:17:53 -0400

    controller done for the moment

--- src/main/java/com/amplifier/controllers/UserBlizzardAccountController.java
+++ src/main/java/com/amplifier/controllers/UserBlizzardAccountController.java
@@ -1,10 +1,25 @@
 package com.amplifier.controllers;
 
+import java.util.List;
+
+import static com.amplifier.util.ClientMessageUtil.CREATION_FAILED;
+import static com.amplifier.util.ClientMessageUtil.CREATION_SUCCESSFUL;
+import static com.amplifier.util.ClientMessageUtil.DELETION_FAILED;
+import static com.amplifier.util.ClientMessageUtil.DELETION_SUCCESSFUL;
+import static com.amplifier.util.ClientMessageUtil.UPDATE_FAILED;
+import static com.amplifier.util.ClientMessageUtil.UPDATE_SUCCESSFUL;
+
+import com.amplifier.models.ClientMessage;
 import com.amplifier.models.User;
+import com.amplifier.models.UserBlizzardAccount;
 import com.amplifier.services.UserBlizzardAccountService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,9 +35,33 @@ public class UserBlizzardAccountController {
     private UserBlizzardAccountService service;
 
     @ApiOperation(value = "Find user's blizzard account by id number", notes = "Provide an id to lookup a specific blizzard account from the API", response = User.class)
-    @GetMapping(path = "/blizzard-controller")
-    public @ResponseBody User getById(@RequestParam(value = "id", name = "id") int id) {
-
-        return userService.getUserById(id);
+    @GetMapping(path = "/blizzard-account?id={id}")
+    public @ResponseBody UserBlizzardAccount getById(@RequestParam(value = "id", name = "id") int id) {
+        return service.getById(id);
     }
+
+    @GetMapping("/blizzard-account")
+    @ApiOperation(value = "Find all users' blizzard accounts")
+    public @ResponseBody List<UserBlizzardAccount> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping("/blizzard-account")
+    @ApiOperation(value = "Create new user's blizzard account entity")
+    public @ResponseBody ClientMessage createAccount(@RequestBody UserBlizzardAccount account) {
+        return service.add(account) ? CREATION_SUCCESSFUL : CREATION_FAILED;
+    }
+
+    @PutMapping("/blizzard-account")
+    @ApiOperation(value = "Update user's blizzard account entity")
+    public @ResponseBody ClientMessage updateAccount(@RequestBody UserBlizzardAccount account) {
+        return service.edit(account) ? UPDATE_SUCCESSFUL : UPDATE_FAILED;
+    }
+
+    @DeleteMapping("/blizzard-account")
+    @ApiOperation(value = "Remove user's blizzard account entity")
+    public @ResponseBody ClientMessage deleteAccount(@RequestBody UserBlizzardAccount account) {
+        return service.remove(account.getId()) ? DELETION_SUCCESSFUL : DELETION_FAILED;
+    }
+
 }
