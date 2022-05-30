4afac430096ecb667e0668a88c59063f0cd8264b
FullSol <ceraines@student.fullsail.edu>
2022-05-30 13:38:21 -0400

    changed java version and fixed issues

--- src/main/java/com/amplifier/models/UserRole.java
+++ src/main/java/com/amplifier/models/UserRole.java
@@ -1,13 +1,14 @@
 package com.amplifier.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
--- src/main/java/com/amplifier/repositories/ImgPostRepository.java
+++ src/main/java/com/amplifier/repositories/ImgPostRepository.java
@@ -2,13 +2,13 @@ package com.amplifier.repositories;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import com.amplifier.models.ImgPost;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import jakarta.transaction.Transactional;
 
 @Repository
 @Transactional
