11e100504672990659df150ab1a2c6fa6b1c2287
FullSol <ceraines@student.fullsail.edu>
2022-05-30 17:09:28 -0400

    corrected UserRole endpoint

--- src/main/java/com/amplifier/controllers/UserRolesController.java
+++ src/main/java/com/amplifier/controllers/UserRolesController.java
@@ -1,11 +1,11 @@
 package com.amplifier.controllers;
 
-import static com.amplifier.util.ClientMessageUtil.CREATION_SUCCESSFUL;
 import static com.amplifier.util.ClientMessageUtil.CREATION_FAILED;
-import static com.amplifier.util.ClientMessageUtil.UPDATE_FAILED;
-import static com.amplifier.util.ClientMessageUtil.UPDATE_SUCCESSFUL;
+import static com.amplifier.util.ClientMessageUtil.CREATION_SUCCESSFUL;
 import static com.amplifier.util.ClientMessageUtil.DELETION_FAILED;
 import static com.amplifier.util.ClientMessageUtil.DELETION_SUCCESSFUL;
+import static com.amplifier.util.ClientMessageUtil.UPDATE_FAILED;
+import static com.amplifier.util.ClientMessageUtil.UPDATE_SUCCESSFUL;
 
 import java.util.List;
 
@@ -14,7 +14,6 @@ import com.amplifier.models.UserRole;
 import com.amplifier.services.UserRolesService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -48,9 +47,9 @@ public class UserRolesController {
         return service.getById(id);
     }
 
-    @PostMapping(path = "/role", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
+    @PostMapping(path = "/role")
     @ApiOperation(value = "Create new user role entity", notes = "Add a new user role in the API.")
-    public @ResponseBody ClientMessage createUserRole(UserRole userRole) throws Exception {
+    public @ResponseBody ClientMessage createUserRole(@RequestBody UserRole userRole) throws Exception {
         return service.add(userRole) ? CREATION_SUCCESSFUL : CREATION_FAILED;
     }
 
